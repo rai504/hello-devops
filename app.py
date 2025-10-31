cfa4dbc7e5eee1af3f8f5c0abefcc3771b1ffd87
Deepak Rai <rai.deepak.deepak@gmail.com>
2025-10-31 10:52:47 +0100

    appended bhais ke sath message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, DevOps Check!"
+    return "Hello, DevOps Check! Bhais ke saath!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
